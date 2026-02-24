3126376369269a08bc6fb4fe6937e1a344266209
d-weimer <weimer0120@gmail.com>
2026-02-24 14:59:16 +0900

    Implemented checkbox & delete button listeners

--- components/Todo.js
+++ components/Todo.js
@@ -4,21 +4,38 @@ class Todo {
     this._templateElement = document.querySelector(selector);
   }
 
+  _generateCheckboxEl() {
+    this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+    this._todoLabel = this._todoElement.querySelector(".todo__label");
+
+    this._todoCheckboxEl.checked = this._data.completed;
+
+    this._todoCheckboxEl.id = `todo-${this._data.id}`;
+    this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
+  }
+
+  _setEventListeners() {
+    this._todoDeleteBtn.addEventListener("click", () => {
+      this._todoElement.remove();
+    });
+
+    this._todoCheckboxEl.addEventListener("change", () => {
+      this._data.completed = !this._data.completed;
+    });
+  }
+
   getView() {
     this._todoElement = this._templateElement.content
       .querySelector(".todo")
       .cloneNode(true);
     const todoNameEl = this._todoElement.querySelector(".todo__name");
-    const todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
-    const todoLabel = this._todoElement.querySelector(".todo__label");
     const todoDate = this._todoElement.querySelector(".todo__date");
-    const todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+    this._todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
 
     todoNameEl.textContent = this._data.name;
-    todoCheckboxEl.checked = this._data.completed;
 
-    todoCheckboxEl.id = `todo-${this._data.id}`;
-    todoLabel.setAttribute("for", `todo-${this._data.id}`);
+    this._generateCheckboxEl();
+    this._setEventListeners();
 
     const dueDate = new Date(this._data.date);
     if (!isNaN(dueDate)) {
@@ -28,10 +45,6 @@ class Todo {
         day: "numeric",
       })}`;
     }
-
-    todoDeleteBtn.addEventListener("click", () => {
-      todoElement.remove();
-    });
 
     return this._todoElement;
   }
