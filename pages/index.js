084aecf07debb80df9dd4d4f0ebbf9f7fca74e4f
d-weimer <weimer0120@gmail.com>
2026-02-24 17:33:50 +0900

    removed comment

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,6 @@ const closeModal = (modal) => {
   modal.classList.remove("popup_visible");
 };
 
-// The logic in this function should all be handled in the Todo class.
 const generateTodo = (data) => {
   const todo = new Todo(data, "#todo-template");
   const todoElement = todo.getView();
